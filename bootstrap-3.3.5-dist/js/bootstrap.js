f7240cf66c86f4abcaa0de1960fb4958bb1fb628
Łukasz Sroczyński <Surreallistic@users.noreply.github.com>
2016-11-11 18:18:20 +0100

    Update bootstrap.js

--- bootstrap-3.3.5-dist/js/bootstrap.js
+++ bootstrap-3.3.5-dist/js/bootstrap.js
@@ -16,6 +16,7 @@ if (typeof jQuery === 'undefined') {
   }
 }(jQuery);
 
+
 /* ========================================================================
  * Bootstrap: transition.js v3.3.5
  * http://getbootstrap.com/javascript/#transitions
@@ -23,6 +24,7 @@ if (typeof jQuery === 'undefined') {
  * Copyright 2011-2015 Twitter, Inc.
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
  * ======================================================================== */
+
 
 
 +function ($) {
